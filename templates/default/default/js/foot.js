12f86ab6e09af8f02b52846312994016391f7945
Kim-Wan <1063607114@qq.com>
2015-04-02 12:26:35 +0800

    首次导入

---
+++ templates/default/default/js/foot.js
@@ -0,0 +1,3 @@
+var foot='<div id="footWrapx" style="margin-top:-90px"><div class="row-fluid"><div id="footer" class="span12">&copy;2014 <a href="#">XXX网络科技有限公司</a> 版权所有</div></div></div>';
+document.getElementById("footWrapx").innerHTML=footWrapxHTML;
+
